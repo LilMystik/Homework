65b1b65aa5d3c484532eb14e23e003d20e8bafef
LilMystik <160438341+LilMystik@users.noreply.github.com>
2025-04-15 18:54:51 +0300

    Update SpringConfig.java

--- src/main/java/identifier/config/SpringConfig.java
+++ src/main/java/identifier/config/SpringConfig.java
@@ -26,7 +26,7 @@ public class SpringConfig {
     dataSource.setDriverClassName("org.postgresql.Driver");
     dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
     dataSource.setUsername("postgres");
-    dataSource.setPassword("FayeValentine3002");
+    dataSource.setPassword("postgres");
     return dataSource;
   }
 
