8d46def7720d3609019d669fdfcb1b5eb2ee6a74
LilMystik <160438341+LilMystik@users.noreply.github.com>
2025-04-15 19:01:08 +0300

    Delete src/main/java directory

--- src/main/java/identifier/exception/WorkspaceUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package identifier.exception;
-
-public class WorkspaceUnavailableException extends Exception {
-  public WorkspaceUnavailableException(String message) {
-    super(message);
-  }
-}
--- src/main/java/identifier/repository/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package identifier.repository;
-
-import identifier.models.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
-}
